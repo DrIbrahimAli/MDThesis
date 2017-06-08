34cef3afb673140ea29b82e7244a4c1d78b759dc
DrIbrahimAli <dr.ibrahim.nofel@gmail.com>
2017-06-08 19:34:31 +0200

    writing discussion

--- ref.py
+++ ref.py
@@ -3,19 +3,24 @@ import sys
 fh = open(sys.argv[1], 'r')
 
 for line in fh:
+
     info=line.split('.')
+    # print(info)
     authors=info[0]
+    # print(authors)
+    # print(authors.split(', '))
     author=authors.split(',')[0]
     abbrev = author.split(' ')[0]
     title= info[1]
     journal=info[2]
     data=info[3]
+    # print(data)
     reauthor= ''
     for author in authors.split(', '):
-        a, b = author.split(' ')
-        reauthor = reauthor + b + ' ' + a + ' and '
+        *a, b = author.split(' ')
+        reauthor = reauthor + b + ' ' + ' '.join(a) + ' and '
     authors = reauthor[:-5]
-    year, data = data.split(';')
+    year, data = data.split(',')
     volume, pages = data.split(':')
     abbrev = (abbrev + year).replace(' ','')
     print('@article{'+abbrev+',')
